c93fff084fd8086e1e63b6ce8d63d96bbc63f4c5
Mathieu Bouchard <matb33@gmail.com>
2015-09-09 09:23:38 -0500

    add w h

--- www/PhotoLibrary.js
+++ www/PhotoLibrary.js
@@ -3,7 +3,7 @@ console.log("[PhotoLibrary] installing...");
 var exec = require("cordova/exec");
 
 module.exports = {
-	getRandomPhotos: function (howMany, callback) {
+	getRandomPhotos: function (howMany, width, height, callback) {
 		console.log("[PhotoLibrary] getRandomPhotos");
 
 		if (navigator.userAgent.indexOf("Android") !== -1) {
@@ -19,7 +19,7 @@ module.exports = {
 				});
 			}, function error(err) {
 				callback(err, {});
-			}, "PhotoLibrary", "getRandomPhotos", [howMany]);
+			}, "PhotoLibrary", "getRandomPhotos", [howMany, width, height]);
 		}
 	}
 };
