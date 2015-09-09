4fc6f8e00c77ecebd3334e8e539deef7cfd240e8
Mathieu Bouchard <matb33@gmail.com>
2015-09-08 22:30:34 -0500

    kind of working

--- src/ios/PhotoLibrary.m
+++ src/ios/PhotoLibrary.m
@@ -18,32 +18,73 @@
 }
 
 - (void)getRandomPhotos:(CDVInvokedUrlCommand*)command {
-    NSNumber* howMany = [command argumentAtIndex:0 withDefault:nil];
-
+    NSNumber* howMany = [command argumentAtIndex:0];
+    
+    NSLog(@"[getRandomPhotos] howMany: %@", howMany);
+    
     [self.commandDelegate runInBackground:^{
         PHFetchResult *fetchResult = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:nil];
 
         // generate howMany randomIndexes with a maximum based on fetchResult.count
-        NSMutableIndexSet *randomIndexes = [NSMutableIndexSet indexSet];
-        for (int i = 0; i < (int)howMany; i++) {
+        NSMutableIndexSet *randomIndexes = [[NSMutableIndexSet alloc] init];
+        for (int i = 0; i < [howMany intValue]; i++) {
             [randomIndexes addIndex:[self getRandomNumber:fetchResult.count]];
         }
-
+        
+        NSLog(@"[getRandomPhotos] randomIndexes: %@", randomIndexes);
+        
         NSMutableArray *randomPhotos = [[NSMutableArray alloc] init];
-        [fetchResult enumerateObjectsAtIndexes:randomIndexes options:NSEnumerationConcurrent usingBlock:^(PHAsset *asset, NSUInteger idx, BOOL *stop) {
+        [fetchResult enumerateObjectsAtIndexes:randomIndexes options:0 usingBlock:^(PHAsset *asset, NSUInteger idx, BOOL *stop) {
+            NSLog(@"[getRandomPhotos] asset: %@", asset);
+            
+            PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+            options.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+            options.synchronous = YES;
+            options.networkAccessAllowed = NO;
+            
             [[PHImageManager defaultManager] requestImageForAsset:asset
-                                             targetSize:CGSizeMake(1920, 1080)
-                                             contentMode:PHImageContentModeAspectFill
-                                             options:PHImageRequestOptionsVersionCurrent
-                                             resultHandler:^(UIImage *result, NSDictionary *info) {
-                                                 NSURL* imageURL = [info objectForKey:UIImagePickerControllerReferenceURL];
-                                                [randomPhotos addObject:imageURL];
-                                             }];
+                                                       targetSize:CGSizeMake(1920, 1080)
+                                                      contentMode:PHImageContentModeAspectFill
+                                                          options:options
+                                                    resultHandler:^(UIImage *result, NSDictionary *info) {
+                                                         NSURL *imageFileURL = [info objectForKey:@"PHImageFileURLKey"];
+                                                        NSString *imageURL = [imageFileURL relativePath];
+                                                        NSLog(@"[getRandomPhotos] imageURL: %@", imageURL);
+                                                        [randomPhotos addObject:imageURL];
+                                                    }];
         }];
-
+        
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsMultipart:randomPhotos];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
 
 @end
+
+/*
+ - (void)showSquareImageForAsset:(PHAsset *)asset
+ {
+ NSInteger retinaScale = [UIScreen mainScreen].scale;
+ CGSize retinaSquare = CGSizeMake(100*retinaScale, 100*retinaScale);
+ 
+ PHImageRequestOptions *cropToSquare = [[PHImageRequestOptions alloc] init];
+ cropToSquare.resizeMode = PHImageRequestOptionsResizeModeExact;
+ 
+ CGFloat cropSideLength = MIN(asset.pixelWidth, asset.pixelHeight);
+ CGRect square = CGRectMake(0, 0, cropSideLength, cropSideLength);
+ CGRect cropRect = CGRectApplyAffineTransform(square,
+ CGAffineTransformMakeScale(1.0 / asset.pixelWidth,
+ 1.0 / asset.pixelHeight));
+ 
+ cropToSquare.normalizedCropRect = cropRect;
+ 
+ [[PHImageManager defaultManager]
+ requestImageForAsset:(PHAsset *)asset
+ targetSize:retinaSquare
+ contentMode:PHImageContentModeAspectFit
+ options:cropToSquare
+ resultHandler:^(UIImage *result, NSDictionary *info) {
+ self.imageView.image = result;
+ }];
+ }
+*/
